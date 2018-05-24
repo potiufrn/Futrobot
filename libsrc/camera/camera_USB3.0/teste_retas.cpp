4e7392bfd35e2a965e8877f3a653d3e39da65b10
poti <juliomaia@ufrn.edu.br>
2018-05-24 18:08:49 -0300

    Atualização Histograma

--- libsrc/camera/camera_USB3.0/teste_retas.cpp
+++ libsrc/camera/camera_USB3.0/teste_retas.cpp
@@ -23,8 +23,8 @@ public:
       histoB[i] = 0;
     }
 
-    for(unsigned i = 0; i<dest.nlin(); i++)
-      for(unsigned j = 0; j<dest.ncol(); j++){
+    for(unsigned i = 120; i<360; i++)
+      for(unsigned j = 160; j<480; j++){
         if( (i%2 == 0) && (j%2 != 0) )
           histoB[dest.getPixel(i,j)] +=1;
         else if( (i%2 != 0) && (j%2 == 0) )
@@ -50,24 +50,33 @@ public:
   }
 };
 
+void segmentacao (ImagemGBRG &dest, uint8_t ref){
+  for(unsigned i = 0; i<dest.nlin(); i++)
+  for(unsigned j = 0; j<dest.ncol(); j++){
+    if(dest(i,j) < ref) dest(i,j) = (uint8_t)0;
+    else
+    dest(i,j) = (uint8_t)255;
+  }
+};
+
 int main(){
   TesteCam cam(1);
-  ImagemRGB imrgb(0,0);
+  ImagemRGB imrgb(0,0), segRGB(0,0);
   char key;
 
-  int gain = 77;
-  int ExpAbs = 292;
-
-  struct controler qGain;
-  struct controler qExpAbs;
-
-  if(!cam.queryGain(qGain) ) std::cout << "Camera nao possui o controles" << '\n';
-  else std::cout << "Gain min - max " <<qGain.min << ' ' <<qGain.max << '\n';
-  if ( !cam.setGain(gain) ) std::cout << "falha na alteracao dos controles" << '\n';
-
-  if(!cam.queryExposureAbs(qExpAbs) ) std::cout << "Camera nao possui o controles" << '\n';
-  else  std::cout << "Exposure(Absolute) min  - max " <<qExpAbs.min << ' ' <<qExpAbs.max << '\n';
-  if ( !cam.setExposureAbs( ExpAbs ) ) std::cout << "falha na alteracao do controle" << '\n';
+  // int gain = 77;
+  // int ExpAbs = 292;
+  //
+  // struct controler qGain;
+  // struct controler qExpAbs;
+  //
+  // if(!cam.queryGain(qGain) ) std::cout << "Camera nao possui o controles" << '\n';
+  // else std::cout << "Gain min - max " <<qGain.min << ' ' <<qGain.max << '\n';
+  // if ( !cam.setGain(gain) ) std::cout << "falha na alteracao dos controles" << '\n';
+  //
+  // if(!cam.queryExposureAbs(qExpAbs) ) std::cout << "Camera nao possui o controles" << '\n';
+  // else  std::cout << "Exposure(Absolute) min  - max " <<qExpAbs.min << ' ' <<qExpAbs.max << '\n';
+  // if ( !cam.setExposureAbs( ExpAbs ) ) std::cout << "falha na alteracao do controle" << '\n';
 
 
   while(true){
@@ -92,9 +101,12 @@ int main(){
       cam.toRGB(imrgb);
       double end_2RGB = relogio();
 
-      imrgb.save("RGBtest.ppm");
+      // imrgb.save("RGBtest.ppm");
       cam.histogram(Imgbruta);
-
+      segmentacao(Imgbruta, 255);
+      Imgbruta.save("GBRG_seg.ppm");
+      Imgbruta.toImageRGB(segRGB);
+      segRGB.save("segmentada.ppm");
       cout << "Captura time : " << end - start << endl;
       cout << "To RGB time  : " << end_2RGB - start_2RGB << endl;
 
