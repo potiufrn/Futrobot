793a35253a9980ea0228f5e90068aa57ff74936e
Luís Gabriel <gabriellgpc@ufrn.edu.br>
2019-05-24 10:44:29 -0300

    inicio da documentação do acquisition

--- program/main/acquisition.h
+++ program/main/acquisition.h
@@ -13,7 +13,7 @@
 #endif
 
 
-enum REG_COLOR {
+enum  REG_COLOR {
   REG_COLOR_ORANGE =0,
   REG_COLOR_BLUE   =1,
   REG_COLOR_YELLOW =2,
@@ -28,6 +28,7 @@ enum REG_COLOR {
   REG_COLOR_UNDEFINED = -1
 };
 
+
 struct REGION{
   Coord2 center;
   double orientation;
@@ -36,13 +37,6 @@ struct REGION{
   int nPixel;
 };
 
-/**
- * [canBePainted description]
- * @param  colorID [description]
- * @param  u       [description]
- * @param  v       [description]
- * @return         [description]
- */
 class Acquisition :
   public virtual FutData
 #ifndef _SO_SIMULADO_
@@ -60,13 +54,38 @@ private:
   bool saveNextImage;
   //FUNCTIONS
 #ifndef _SO_SIMULADO_
+
+/**
+ * Verifica se o pixel na posicao (u,v) pertence ao intervalo definido para a cor ColorID
+ * @param  colorID Testa se pixel pode ser considerado dessa cor.
+ * @param  u       indice da coluna da imagem.
+ * @param  v       indice da linha da imagem.
+ * @return         true caso seja da cor ColorID, false caso contrario.
+ */
   bool canBePainted(REG_COLOR colorID,unsigned u, unsigned v);
+  /**
+   * Algoritmo otimizado de SEEDFILL/FLOODFILL
+   * dado uma posicao na imagem (semente) e uma cor,
+   * o algoritmo encontra todos os pixels associados a essa
+   * semente e sobrescreve os valores desses pixels
+   * pela cor especificada.
+   * @param  colorID Cor que sera aplicada na regiao.
+   * @param  u       indice da coluna na imagem.
+   * @param  v       indice da linha na imagem.
+   * @return         Retorna uma estrutura REGION.
+   */
   REGION seedFill( REG_COLOR colorID, unsigned int u, unsigned int v);
+  /**
+   * Metodo que realiza os algoritmos de visao computacional,
+   * identica a regiao, posicao central e theta, de cada robo e
+   * localizacao da bola.
+   * @return retorna true em caso de erro.
+   */
   bool processGameState();
 #endif
   /*************************************************************************************
    * Interpeta o estado atual do jogo apartir da leitura via socket                    *
-   * @return [true em caso de erro]                                                    *
+   * @return true em caso de erro
    *************************************************************************************/
   bool readGameState();
   /**
